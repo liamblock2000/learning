e276a753586f32bd1911b254175f1db11c440823
Liam Block <61854042+liamblock2000@users.noreply.github.com>
2021-01-16 12:09:52 -0800

    For Loop
    Finished: Add For Loop, print out text [1,2,3,4,5,6,7]
    To-do:  Add Line Space every time there is a [1,2,3,4,5,6,7]
    Extra: Add spaces between numbers

--- learn/learn/ArrayTools.cs
+++ learn/learn/ArrayTools.cs
@@ -62,18 +62,16 @@ namespace learn
         {
             // use a look to create this output to the console
             // whith array [1,2,3,4,5,6,7] print out to the screen [1,2,3,4,5,6]
-            
-            for(...)
+
+            for (int i = 0; i < array.Length; i++)
             {
-
+                Console.Write("[");
+                Console.Write($"{1,2,3,4,5,6,7}");
+                Console.WriteLine($"{array[1]}");
+                //Console.WriteLine($"{array[3]}");           
+                //Console.Write($"{array[1]}");
             }
-            Console.Write("[");
-            Console.Write($"{array[0]}");
-            Console.Write($"{array[1]}");
-            Console.WriteLine($"{array[2]}");
-            Console.WriteLine($"{array[3]}");
 
-            // Print the array
         }
 
     }
