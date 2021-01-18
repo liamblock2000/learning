54a899198251049092cfa78324d8a60b97986415
Liam Block <61854042+liamblock2000@users.noreply.github.com>
2021-01-18 14:23:20 -0800

    Matrix 6-8

--- learn/learn/MatrixTools.cs
+++ learn/learn/MatrixTools.cs
@@ -5,29 +5,18 @@ namespace learn
 {
     class MatrixTools
     {
-        
         public void Print(int[,] matrix)
         {
-
-            // 1 ... 10
-            // 11... 20
-            //    ...
-            // 91 ... 100
-           
-            Console.Write("[");
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine("");
                 for (int j = 0; j < 10; j++)
                 {
-
-                    Console.Write(",\t");
+                    Console.Write(" ");
                     Console.Write(matrix[i,j]);
-
                 }
             
             }
-            Console.Write("]");
         }
         public void Fill1(int[,] matrix)
         {
@@ -49,7 +38,6 @@ namespace learn
         }
         public void FillArray3(int[,] arr)
         {
-            
             for (int i = 0; i < arr.Length; i++)
             {
                 for (int j = 0; j < arr.Length; j++)
@@ -57,27 +45,104 @@ namespace learn
                 {
                     arr[i,j] = i + j; 
                 }
-                    
-                
-                
             }
         }
         public void FillArray4(int[,] arr)
         {
-
-            for (int i = 0; i < arr.Length; i++)
+            for (int i = 0; i < 10; i++)
             {
-                for (int j = 0; j < arr.Length; j++)
-
+                for (int j = 0; j < 10; j++)
                 {
-                    arr[i, j] = i + j;
+                    arr[i, j] = (j+i)%2;
                 }
-
-
-
             }
         }
 
+
+
+        public void FillArray5(int[,] arr)
+        {
+            // 1 0 0 0
+            // 0 1 0 0
+            // 0 0 1 0
+            // 0 0 0 1
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if(i ==j )
+                    {
+                        arr[i, j] = 1;
+                    }
+                    else
+                    {
+                        arr[i, j] = 0;
+                    }
+                    
+                }
+            }
+        }
+
+        public void FillArray6(int[,] arr)
+        {
+            // 0 0 0 1
+            // 0 0 1 0
+            // 0 1 0 0
+            // 1 0 0 0
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    arr[i, j] = (i+j)== 9 ? 1 : 0;
+                }
+            }
+        }
+
+        public void FillArray7(int[,] arr)
+        {
+            // 1 1 1 1
+            // 1 0 0 1
+            // 1 0 0 1
+            // 1 1 1 1
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (i == 0 || j == 9)
+                    {
+                        arr[i, j] = 1;
+                    }
+                    else
+                    {
+                        arr[i, j] = 0;
+                    }
+                }
+            }
+        }
+
+
+        public void FillArray8(int[,] arr)
+        {
+            // 0 1 1 1
+            // 1 0 1 1
+            // 1 1 0 1
+            // 1 1 1 0
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if(i !=j )
+                    {
+                        arr[i, j] = 1;
+                    }
+                    else
+                    {
+                        arr[i, j] = 0;
+                    }
+                    
+                }
+            }
+        }
 
 
 
--- learn/learn/Program.cs
+++ learn/learn/Program.cs
@@ -4,13 +4,11 @@ namespace learn
 {
     class Program
     {
-
-
         static void Main()
         {
             MatrixTools m = new MatrixTools();
             int[,] matrix = new int[10, 10];
-            m.Fill1(matrix);
+            m.FillArray7(matrix);
             m.Print(matrix);
         }      
     }
